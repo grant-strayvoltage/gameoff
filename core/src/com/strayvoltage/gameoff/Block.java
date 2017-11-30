886dca7b481a3c5cc1dd6aa6aeffd55761ca0af1
grant-strayvoltage <grant@fivemedia.ca>
2017-11-30 15:55:48 -0400

    corrected little guy jumping too high and added a bigger block if needed

--- core/src/com/strayvoltage/gameoff/Block.java
+++ core/src/com/strayvoltage/gameoff/Block.java
@@ -8,21 +8,25 @@ public class Block extends GameMapObject {
 
   public void init(MapProperties mp, TextureAtlas textures)
   {
-	m_categoryBits = Box2dVars.BLOCK;
-	m_filterMask =Box2dVars.SWITCH | Box2dVars.OBJECT| Box2dVars.BLOCK | Box2dVars.PLAYER_FOOT | 
+	  m_categoryBits = Box2dVars.BLOCK;
+	  m_filterMask =Box2dVars.SWITCH | Box2dVars.OBJECT| Box2dVars.BLOCK | Box2dVars.PLAYER_FOOT | 
 					Box2dVars.POWER | Box2dVars.BRAIN_FOOT | Box2dVars.PLATFORM | Box2dVars.PLAYER_NORMAL | 
-					Box2dVars.PLAYER_JUMPING | Box2dVars.FLOOR| Box2dVars.HAZARD | Box2dVars.FAN;
+					Box2dVars.PLAYER_JUMPING | Box2dVars.PLATFORM_STOP| Box2dVars.FLOOR|Box2dVars.HAZARD;
     m_density = 3.0f;
     m_colBits = 3;
     TextureRegion texture = null;
-    texture = textures.findRegion("block1_moveable");
+
+    if (mp.get("width", Float.class) > 32f)
+      texture = textures.findRegion("block2_moveable");
+    else
+      texture = textures.findRegion("block1_moveable");
+
     this.setRegion(texture);
-    this.setSize(texture.getRegionWidth()-1,texture.getRegionHeight()-1);
+    this.setSize(texture.getRegionWidth(),texture.getRegionHeight());
   }
 
   public void update(float deltaTime)
   {
-	  m_body.setActive(true);
       this.setPositionToBody();
   }
 
